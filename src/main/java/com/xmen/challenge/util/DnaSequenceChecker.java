4eb323fcbf4725cd872b6e71b11c7fd81b901f51
GabrielHedric <gabriel.hedric@gmail.com>
2024-10-24 00:00:50 -0300

    refactor: refactoring code using Streams for better readability

--- src/main/java/com/xmen/challenge/util/DnaSequenceChecker.java
+++ src/main/java/com/xmen/challenge/util/DnaSequenceChecker.java
@@ -3,80 +3,41 @@ package com.xmen.challenge.util;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.stream.IntStream;
 
 @Component
 public class DnaSequenceChecker {
 
     public boolean hasHorizontalSequence(char[][] dnaMutant, int size){
-        for (int row = 0; row < size; row++){
-            for (int col = 0; col < size - 3; col++){
-                boolean result = true;
-                for(int k = 0; k < 4; k++) {
-                    if(dnaMutant[row][col + k] != dnaMutant[row][col]){
-                        result = false;
-                        break;
-                    }
-                }
-                if(result){
-                    return true;
-                }
-            }
-        }
-        return false;
+        return IntStream.range(0, size).anyMatch(row ->
+                IntStream.range(0, size - 3).anyMatch(col ->
+                        IntStream.range(0, 4).allMatch(k -> dnaMutant[row][col + k] == dnaMutant[row][col])
+                )
+        );
     }
 
     public boolean hasVerticalSequence(char[][] dnaMutant, int size){
-        for(int col = 0; col < size; col++){
-            for(int row = 0; row < size -3; row++){
-                boolean result = true;
-                for(int k = 0; k < 4; k++) {
-                    if(dnaMutant[row + k][col] != dnaMutant[row][col]){
-                        result = false;
-                        break;
-                    }
-                }
-                if(result){
-                    return true;
-                }
-            }
-        }
-        return false;
+        return IntStream.range(0, size).anyMatch(row ->
+                IntStream.range(0, size - 3).anyMatch(col ->
+                        IntStream.range(0, 4).allMatch(k -> dnaMutant[row][col + k] == dnaMutant[row][col])
+                )
+        );
     }
 
     public boolean hasDescendingDiagonalSequence(char[][] dnaMutant, int size){
-        for (int row = 0; row < size - 3; row++){
-            for(int col = 0; col < size - 3; col++){
-                boolean result = true;
-                for(int k = 0; k < 4; k++){
-                    if(dnaMutant[row + k][col + k] != dnaMutant[row][col]){
-                        result = false;
-                        break;
-                    }
-                }
-                if(result){
-                    return true;
-                }
-            }
-        }
-        return false;
+        return IntStream.range(0, size - 3).anyMatch(row ->
+                IntStream.range(0, size - 3).anyMatch(col ->
+                        IntStream.range(0, 4).allMatch(k -> dnaMutant[row + k][col + k] == dnaMutant[row][col])
+                )
+        );
     }
 
     public boolean hasAscendingDiagonalSequence(char[][] dnaMutant, int size){
-        for (int row = 3; row < size; row++){
-            for(int col = 0; col < size - 3; col++){
-                boolean result = true;
-                for(int k = 0; k < 4; k++){
-                    if(dnaMutant[row - k][col + k] != dnaMutant[row][col]){
-                        result = false;
-                        break;
-                    }
-                }
-                if(result){
-                    return true;
-                }
-            }
-        }
-        return false;
+        return IntStream.range(3, size).anyMatch(row ->
+                IntStream.range(0, size - 3).anyMatch(col ->
+                        IntStream.range(0, 4).allMatch(k -> dnaMutant[row - k][col + k] == dnaMutant[row][col])
+                )
+        );
     }
 
     public boolean isValidDna(List<String> dnaList){
